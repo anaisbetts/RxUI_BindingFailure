11462ed0e915ed92357dfe09f610b864e0b68c34
phil <pcleveland@sonocine.com>
2014-05-21 11:46:21 -0700

    initial commit

---
+++ RxUI_BindingFailure/MainWindowView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ReactiveUI;
+using ReactiveUI.Xaml;
+using System;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RxUI_BindingFailure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window, IViewFor<MainWindowViewModel>
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainWindowViewModel();
+            this.DataContext = ViewModel;
+
+            //this isn't working as I would expect. Am I Doing it Wrong?
+            this.WhenAnyValue(x => x.items.SelectedValue)
+                .Select(x => x == null ? string.Empty : x.ToString())
+                .BindTo(this, x => x.ViewModel.SelectedItem);
+
+            //if the above binding works then the selected text should show up
+            this.OneWayBind(ViewModel, vm => vm.SelectedItem, v => v.theSelectedItem.Text);
+            //if the above binding works then the button should get enabled.
+            this.BindCommand(ViewModel, vm => vm.GoToCmd, v => v.goTo);
+
+
+            //this is not working as I would expect. If I remove the xaml binding nothing shows up.
+            //this.OneWayBind(ViewModel, vm => vm.Items, v=>v.items.ItemsSource);
+
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainWindowViewModel), typeof(MainWindowView), new PropertyMetadata(null));
+
+        public MainWindowViewModel ViewModel
+        {
+            get { return (MainWindowViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainWindowViewModel)value; }
+        }
+    }
+}
---
+++ RxUI_BindingFailure/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxUI_BindingFailure
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+
+        public MainWindowViewModel()
+        {
+            Items = new ReactiveList<string>(new List<string> { "Item1", "Item2", "Item3" });
+            var canGoTo = this.WhenAny(vm => vm.SelectedItem, x => !string.IsNullOrEmpty(x.Value));
+            GoToCmd = new ReactiveCommand(canGoTo);
+            GoToCmd.Subscribe(_ => { Console.WriteLine("I made it here, so the SelectedItem binding must be fixed."); });
+        }
+
+        public ReactiveCommand GoToCmd { get; protected set; }
+        public ReactiveList<string> Items { get; protected set; }
+
+        private string _SelectedItem;
+        public string SelectedItem
+        {
+            get { return _SelectedItem; }
+            set { this.RaiseAndSetIfChanged(ref _SelectedItem, value); }
+        }
+    }
+}
